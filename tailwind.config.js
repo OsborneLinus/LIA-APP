1493883e24c406d0c7145b36cb59994abe6d71f7
sirisayshello <veckovis.salongen0p@icloud.com>
2024-03-20 14:48:42 +0100

    font fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        inter: ['Inter']
+      },
+      colors: {
+        'gothenburg-grey' : '#4f4f4f',
+        'bananpiren-yellow': '#FCFA58;',
+        'yrgo-red': '#F52A3B'
+      },
+    },
   },
   plugins: [],
 };
