bfbe47d0215e7cf5ba968ab014cbe35554b3cada
sirisayshello <veckovis.salongen0p@icloud.com>
2024-03-25 14:57:38 +0100

    add costum color

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,9 +20,10 @@ module.exports = {
         "gothenburg-grey": "#4f4f4f",
         "bananpiren-yellow": "#FCFA58;",
         "yrgo-red": "#F52A3B",
-        "input-grey": "#f2f2f2"
+        "input-grey": "#f2f2f2",
+        "asphalt-grey": "#333"
       },
     },
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/forms')],
 };
