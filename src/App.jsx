4c86bbc0ef406fedf9ee5b483c9b67d6ee80473f
sirisayshello <veckovis.salongen0p@icloud.com>
2024-03-25 12:15:38 +0100

    input and checkbox components

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Hero from "./Kebab.jsx";
 import Header from "./Header.jsx";
-import Form from "./components/Form.jsx";
+import Form from "./components/Form/Form.jsx";
 import Login from "./services/login";
 import supabase from "./services/supabase";
 
---
+++ src/components/Form/CheckBoxContainer.jsx
@@ -0,0 +1,7 @@
+export const CheckBoxContainer = ({ children }) => {
+  return (
+    <div className="flex gap-4 bg-input-grey text-black py-2 px-4">
+      {children}
+    </div>
+  );
+};
