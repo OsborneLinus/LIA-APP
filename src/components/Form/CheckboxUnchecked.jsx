f39731917ad254006376c33471eba3584dafb092
Adam Welin <welinadam@users.noreply.github.com>
2024-03-26 11:32:32 +0100

    Add custom checkbox svgs

---
+++ src/components/Form/CheckboxUnchecked.jsx
@@ -0,0 +1,17 @@
+function CheckboxUnchecked() {
+  return (
+    <svg
+      className="w-6 h-6 peer-checked:hidden absolute pointer-events-none"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      stroke="#333333"
+    >
+      <rect x="0.5" y="0.5" width="23" height="23" stroke="#333333" />
+    </svg>
+  );
+}
+
+export default CheckboxUnchecked;
