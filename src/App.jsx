820502e46a348cf76c87185d03b37d01edd6dce9
Adam Welin <welinadam@users.noreply.github.com>
2024-03-26 16:17:14 +0100

    Uncomment components in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
 import Hero from "./Hero.jsx";
 import Header from "./Header.jsx";
 import Form from "./components/Form/Form.jsx";
-import { LoginForm } from "./services/Login.jsx";
 import supabase from "./services/supabase";
 import ConfirmationPage from "./components/Form/ConfirmationPage.jsx";
 
 export default function App() {
   return (
     <>
-      {/*       <Header />
+      <Header />
       <Hero />
-      <Form /> */}
-      <LoginForm />
+      <Form />
     </>
   );
 }
