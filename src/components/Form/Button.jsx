f579f1eca9caf1c59e1bab917975c2554f1d715f
OsborneLinus <linusholm1997@gmail.com>
2024-03-26 15:18:08 +0100

    The login form is styled, logic not created yet.

--- src/components/Form/Button.jsx
+++ src/components/Form/Button.jsx
@@ -2,7 +2,7 @@ export const Button = ({ children, type }) => {
   return (
     <button
       type={type}
-      className="bg-asphalt-grey px-4 py-2 flex justify-center items-center text-2xl"
+      className="bg-sky-950 rounded px-4 py-2 flex justify-center items-center text-white text-2xl w-fit"
     >
       {children}
     </button>
--- src/services/login.jsx
+++ src/services/login.jsx
@@ -1,85 +1,71 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { TextInput } from "../components/Form/TextInput";
-import supabase from "../services/supabase";
+import logo from "../assets/YrgoRed.png";
+import App from "../App";
+import { Button } from "../components/Form/Button";
 
-export const Login = () => {
+import supabase from "./supabase";
+
+export const LoginForm = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [session, setSession] = useState(null);
-  const [showSignUp, setShowSignUp] = useState(false);
-
-  useEffect(() => {
-    supabase.auth.getSession().then(({ data: { session } }) => {
-      setSession(session);
-    });
-
-    const {
-      data: { subscription },
-    } = supabase.auth.onAuthStateChange((_event, session) => {
-      setSession(session);
-    });
-
-    return () => subscription.unsubscribe();
-  }, []);
 
   const handleLogin = async (event) => {
     event.preventDefault();
     const { error } = await supabase.auth.signIn({ email, password });
     if (error) console.error("Error logging in:", error.message);
   };
-  const handleSignUp = async (event) => {
-    event.preventDefault();
-    const { error } = await supabase.auth.signUp({ email, password });
-    if (error) console.error("Error logging in:", error.message);
-  };
-
-  if (session) {
-    return <button onClick={() => supabase.auth.signOut()}>Log out</button>;
-  }
 
   return (
-    <div className="flex flex-col justify-center p-12">
-      <form onSubmit={handleLogin}>
-        <h2>Log in</h2>
-        <TextInput
-          id="email"
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          placeholder="Email"
-        />
-        <TextInput
-          id="password"
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          placeholder="Password"
-        />
-        <button type="submit">Log in</button>
-      </form>
-      <button onClick={() => setShowSignUp(!showSignUp)}>
-        {showSignUp ? "Cancel" : "Sign Up"}
-      </button>
-      {showSignUp && (
-        <form onSubmit={handleSignUp}>
-          <h2>Sign up</h2>
+    <div className="flex flex-col">
+      <div className="flex flex-col items-end p-4">
+        <a className="" href="/">
+          <img src={logo} alt="Yrgo logo" onClick={App} />
+        </a>
+      </div>
+      <form className="flex flex-col gap-2" onSubmit={handleLogin}>
+        <div className="p-4">
+          <h1 className="text-5xl font-bold ">LOGGA IN</h1>
+          <p className="inline-flex">Har du inte ett konto?</p>
+          <button className="underline p-2" href="">
+            Skapa ett här!
+          </button>
+        </div>
+        <div className="flex flex-col gap-4 p-4">
+          <label htmlFor="email">E-POST</label>
           <TextInput
-            id="signup-email"
+            id="email"
             type="email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
-            placeholder="Email"
+            placeholder="namn@foretag.com"
           />
+        </div>
+        <div className="flex flex-col gap-4 p-4 pb-12">
+          <label htmlFor="password">LÖSENORD</label>
           <TextInput
-            id="signup-password"
+            id="password"
             type="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
-            placeholder="Password"
+            placeholder="******"
           />
-          <button type="submit">Sign up</button>
-        </form>
-      )}
+        </div>
+        <div className="flex justify-center">
+          <Button type="submit">LOGGA IN</Button>
+        </div>
+      </form>
+      <div className="flex items-center justify-center flex-grow py-12">
+        <div className="w-56 text-center flex flex-col">
+          <a
+            href="/"
+            className="text-black text-base font-light leading-relaxed"
+          >
+            <span className="underline">Läs mer</span> om hur vi hanterar dina
+            personliga uppgifter
+          </a>
+        </div>
+      </div>
     </div>
   );
 };
