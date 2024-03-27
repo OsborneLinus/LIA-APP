51a396410b9d1cc923a446fb935bba5038fa023a
sirisayshello <veckovis.salongen0p@icloud.com>
2024-03-27 22:59:55 +0100

    add FilterDropDown component

--- src/components/Cards/CardContainer.jsx
+++ src/components/Cards/CardContainer.jsx
@@ -1,3 +1,12 @@
+import { FilterDropdown } from "./FilterDropdown";
+
 export const CardContainer = ({ children }) => {
-  return <div className="flex flex-col p-6 gap-6">{children}</div>;
+  return (
+    <div className="flex flex-col ">
+      <div className="flex justify-end px-6">
+        <FilterDropdown />
+      </div>
+      <div className="flex flex-col p-6 gap-6">{children}</div>
+    </div>
+  );
 };
