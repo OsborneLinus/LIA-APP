c30b83f55e8e40aa6e2caa5ddf39aa1a34c9b680
sirisayshello <veckovis.salongen0p@icloud.com>
2024-03-27 11:16:31 +0100

    update file path

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 import supabase from "../../services/supabase";
 import { Button } from "../Common/Button";
 import { CheckBoxContainer } from "./CheckBoxGrid";
-import CheckboxChecked from "./CheckboxChecked";
+import CheckboxChecked from "../../assets/CheckboxChecked";
 import { CheckboxInput } from "./CheckboxInput";
 import CheckboxUnchecked from "./CheckboxUnchecked";
 import ConfirmationPage from "./ConfirmationPage";
