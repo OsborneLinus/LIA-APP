eb9f5c7cb2242c27aaa03800a5d3ff0986d69892
OsborneLinus <linusholm1997@gmail.com>
2024-04-05 10:07:22 +0200

    added grid to the footer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,26 +1,42 @@
 import App from "./App";
 import logo from "/assets/YrgoRed.png";
 import RightArrow from "../public/assets/SidoPil.svg?react";
+import FavoriteHeart from "../public/assets/FavoriteHeart.svg?react";
+import AccountLogo from "../public/assets/accountLogo.svg?react";
 import { useNavigate } from "react-router-dom";
 
 export default function Footer() {
   const navigate = useNavigate();
   return (
-    <div>
-      <div className="inline-flex">
+    <div className="grid grid-cols-3 grid-rows-2 items-center justify-items-center content-center">
+      <div className="row-span-2 ">
         <a className="px-8 pt-8" href="/">
           <img src={logo} alt="Yrgo logo" onClick={App} />
         </a>
       </div>
-      <div className="inline-flex items-center p-6 gap-12">
+      <div className="col-start-2 row-start-1 pt-4">
         <div className="inline-flex">
-          <p>KONTAKT</p>
-          <RightArrow />
+          <FavoriteHeart />{" "}
+          <button className="ml-2" onClick={() => navigate("/userprofile")}>
+            Favoriter
+          </button>
         </div>
-        <div>
-          <p>YRGO 2024</p>
-          <a onClick={() => navigate("/userprofile")}>Favoriter</a>
+      </div>
+      <div className="col-start-3 row-start-1 pt-4">
+        <div className="inline-flex">
+          <AccountLogo />
+          <button className="ml-2 " onClick={() => navigate("/userprofile")}>
+            Konto
+          </button>
         </div>
+      </div>
+
+      <div className="col-start-3 row-start-2 inline-flex">
+        <p>KONTAKT</p>
+        <RightArrow />
+      </div>
+      <div className="col-start-2 row-start-2">
+        <p>YRGO 2024</p>
       </div>
     </div>
   );
