4e35baf0ec195fa584b2309164a5e05cf4a5c137
sirisayshello <veckovis.salongen0p@icloud.com>
2024-04-09 13:33:52 +0200

    fix hero postion for desktop

--- src/Header.jsx
+++ src/Header.jsx
@@ -3,8 +3,7 @@ import logo from "/assets/YrgoRed.png";
 
 export default function Header() {
   return (
-    <div className="flex justify-end p-6">
-      {/* h-[20vh] */}
+    <div className="flex justify-end p-6 md:p-20 w-fit absolute right-0">
       <a href="/">
         <img src={logo} alt="Yrgo logo" onClick={App} />
       </a>
